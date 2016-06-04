853637504776d38d95111d9e5af4fa04400e9ae9
robertov82008 <robertov82008@gmail.com>
2016-06-04 18:32:00 -0300

    add favicon html

--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -5,6 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <!--Let browser know website is optimized for mobile-->
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel=icon href="img/road.gif" sizes="16x16" type="img/gif">
         <title> TransTruck - @yield('titulo') </title>
         <!--Import Google Icon Font-->
         <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
